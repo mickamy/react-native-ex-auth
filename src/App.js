0c2eac4be03c9f6d212f60053ba258757048ba29
Tetsuro Mikami <tetsuro_mikami@wamazing.jp>
2017-07-02 02:11:54 +0900

    Listen to firebase auth change event in App.js

--- src/App.js
+++ src/App.js
@@ -5,6 +5,10 @@ import { Header } from './components/common';
 import LoginForm from './components/LoginForm';
 
 class App extends Component {
+  state = {
+    loggedIn: false,
+  };
+
   componentWillMount() {
     firebase.initializeApp({
       apiKey: 'AIzaSyCVeLEfJg2BzxLAME7SyDWiT0pSMhaHYE0',
@@ -13,6 +17,14 @@ class App extends Component {
       projectId: 'react-native-ex-auth',
       storageBucket: 'react-native-ex-auth.appspot.com',
       messagingSenderId: '1035249633227'
+    });
+
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ loggedIn: true });
+      } else {
+        this.setState({ loggedIn: false });
+      }
     });
   }
 
