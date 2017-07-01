ecb1eba44f013ce9a0854a3e7d36384b2cfd1756
Tetsuro Mikami <tetsuro_mikami@wamazing.jp>
2017-07-02 00:53:32 +0900

    Configure firebase in componentWillMount

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
+import firebase from 'firebase';
 import { Header } from './components/common';
+import LoginForm from './components/LoginForm';
 
 class App extends Component {
+  componentWillMount() {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyCVeLEfJg2BzxLAME7SyDWiT0pSMhaHYE0',
+      authDomain: 'react-native-ex-auth.firebaseapp.com',
+      databaseURL: 'https://react-native-ex-auth.firebaseio.com',
+      projectId: 'react-native-ex-auth',
+      storageBucket: 'react-native-ex-auth.appspot.com',
+      messagingSenderId: '1035249633227'
+    });
+  }
+
   render() {
     return (
       <View>
-        <Header headerText="Authentiation" />
-        <Text>An App!</Text>
+        <Header headerText='Authentiation' />
+        <LoginForm />
       </View>
     );
   }
